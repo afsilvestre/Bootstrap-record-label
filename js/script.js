d38ee5f1d22564c118a68536a27cc832f4f46a45
afsilvestre <andreffsilvestre@gmail.com>
2019-09-01 14:00:45 +0100

    Footer added and files removed

--- js/script.js
+++ js/script.js
@@ -9,4 +9,13 @@
             document.querySelector("header").innerHTML = data;
         });
 
+    //Footer fetch    
+    fetch("./components/footer.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.querySelector("footer").innerHTML = data;
+        });
+
 })();
